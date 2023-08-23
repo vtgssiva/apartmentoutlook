8a4c16754ef3b4de79313d62339a888ad2c90638
i500784 <sivakumar.thiyagarajan@sap.com>
2023-08-23 19:51:10 +0530

    23-08-2023 updated

--- com.exercise/webapp/controller/Home.controller.js
+++ com.exercise/webapp/controller/Home.controller.js
@@ -8,9 +8,21 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("com.exercise.controller.Home", {
-            onInit: function () {
-
-            },
+//            onInit: function () {
+//
+//            },
+//
+//            onBeforeRendering: function () {
+//
+//            },
+//
+//            onAfterRendering: function () {
+//
+//            },
+//
+//            onExit: function () {
+//
+//            },
 
             onItemSelect: function (oEvent) {
                 var oItem = oEvent.getParameter("item");
@@ -29,6 +41,39 @@ sap.ui.define([
                     this.byId("pageContainer2").setVisible(false);
                     this.byId("pageContainer3").setVisible(true);
                 }
+            },
+
+            handleEditableForm: function (oBool) {
+                this.byId("name").setEditable(oBool);
+                this.byId("street").setEditable(oBool);
+                this.byId("housenumber").setEditable(oBool);
+                this.byId("zipcode").setEditable(oBool);
+                this.byId("city").setEditable(oBool);
+                this.byId("country").setEditable(oBool);
+            },
+
+            handleEditPress: function (oEvent) {
+                this.byId("edit").setVisible(false);
+                this.byId("save").setVisible(true);
+                this.byId("cancel").setVisible(true);
+
+                this.handleEditableForm(true);
+            },
+
+            handleCancelPress: function (oEvent) {
+                this.byId("edit").setVisible(true);
+                this.byId("save").setVisible(false);
+                this.byId("cancel").setVisible(false);
+
+                this.handleEditableForm(false);
+            },
+
+            handleSavePress: function (oEvent) {
+                this.byId("edit").setVisible(true);
+                this.byId("save").setVisible(false);
+                this.byId("cancel").setVisible(false);
+
+                this.handleEditableForm(false);
             }
         });
     });
